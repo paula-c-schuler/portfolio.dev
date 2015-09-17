0c4b9b6de58d4e763b26f2f884f63fd6cac6dfac
Paula C Schuler <paulasatx@gmail.com>
2015-09-17 11:59:24 -0500

    SwipeTouch test function not working yet. Portfolio.js loads on the DOM.

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,11 +1,43 @@
 
 $(document).ready(function () {
-	
+	alert("Doc ready");
 
 	// scroll down, after bottom of portfolio section,
 	// displays contact information
 	// fixed in viewport for all scroll down.
 	// scroll up displays none
+	$(function(){
 
+
+	$( window ).scroll(function() {
+  	$( "span" ).css( "display", "inline" ).fadeOut( "slow" );
+	});
+
+	// $( "body" ).scroll(function () {
+  		// if ( $( "p:first" ).is( ":hidden" ) ) {
+  			// alert("is hidden");
+
+    	// $( "p" ).slideDown( "slow" );
+  		// } else {
+    		// $( "p" ).hide();
+ 		// }
+	// });
+
+	});
+
+
+
+	// SwipeTouch Plugin 
+	$(function() {			
+	//Enable swiping...
+	$("#test").swipe( {
+		//Generic swipe handler for all directions
+		swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+			$(this).text("You swiped " + direction );	
+		},
+		//Default is 75px, set to 0 for demo so any distance triggers swipe
+	   threshold:0
+		});
+	});
 
 });
