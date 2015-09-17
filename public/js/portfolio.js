a538c2bf21df39d08749a261f677f6aef42c57d3
Paula C Schuler <paulasatx@gmail.com>
2015-09-17 11:06:05 -0500

    Added TouchSwipe jQuery plugin link and scripts.

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -2,5 +2,10 @@
 $(document).ready(function () {
 	
 
+	// scroll down, after bottom of portfolio section,
+	// displays contact information
+	// fixed in viewport for all scroll down.
+	// scroll up displays none
+
 
 });
