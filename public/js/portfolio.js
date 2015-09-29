de91d187431fa698b19376e4da5f45785cdf6756
Paula C Schuler <paulasatx@gmail.com>
2015-09-29 10:43:51 -0500

    Success with responsive, mobile-first portfolio-box placement.

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function () {
-	alert("Doc ready");
 
 	// scroll down, after bottom of portfolio section,
 	// displays contact information
@@ -9,35 +8,12 @@ $(document).ready(function () {
 	$(function(){
 
 
-	$( window ).scroll(function() {
-  	$( "span" ).css( "display", "inline" ).fadeOut( "slow" );
-	});
-
-	// $( "body" ).scroll(function () {
-  		// if ( $( "p:first" ).is( ":hidden" ) ) {
-  			// alert("is hidden");
-
-    	// $( "p" ).slideDown( "slow" );
-  		// } else {
-    		// $( "p" ).hide();
- 		// }
-	// });
+	
 
 	});
 
 
 
-	// SwipeTouch Plugin 
-	$(function() {			
-	//Enable swiping...
-	$("#test").swipe( {
-		//Generic swipe handler for all directions
-		swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-			$(this).text("You swiped " + direction );	
-		},
-		//Default is 75px, set to 0 for demo so any distance triggers swipe
-	   threshold:0
-		});
-	});
+	
 
 });
